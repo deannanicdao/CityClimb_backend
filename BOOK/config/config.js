0fb62901beb5cfccbf2da8f039c3f9eaddc8a2e6
deannanicdao <deanna.nicdao@gmail.com>
2021-01-28 17:51:27 +1000

    Removed book dir.

--- BOOK/config/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    env: process.env.NODE_ENV || 'start',
-    port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-    mongoUri: process.env.MONGODB_URI || process.env.MONGO_HOST || 'mongodb+srv://Admin:<sAeyi6TUGkDMUUWm>@cityclimb.8vbtl.mongodb.net/<MERN_T3A2>?retryWrites=true&w=majority' + (process.env.IP || 'localhost') + ':' + (process.env.MONGO_PORT || '27017') + '/mernproject'
-}
-
-export default config
