23ae75352cf4833e349ac85474ac7caa1bf5a124
deannanicdao <deanna.nicdao@gmail.com>
2021-01-29 15:57:20 +1000

    DELETE Product route working.

--- routes/products.js
+++ routes/products.js
@@ -47,7 +47,7 @@ router.post("/", [
         try {
             let product = await Product.findOne({ name })
 
-            if(product) {
+            if (product) {
                 response.status(400).json({ errors: [ { msg: 'Product already exists' }] })
             }
 
@@ -68,44 +68,91 @@ router.post("/", [
 )
 
 // UPDATE
-router.put("/:id", (request, response) => {
-    let indexOfTheElement = products.findIndex(element => element.id == request.params.id)
-    let newProduct = request.body
-    products.splice(indexOfTheElement, 1, newProduct)
-    response.send(products[indexOfTheElement])
-})
+// router.put("/:id", [
+//     check('name', 'Name is required').not().isEmpty(),
+//     check('description', 'Description is required').not().isEmpty(),
+//     check('price', 'Please enter a price to three values (0.00)').isLength({ min: 3 })
+// ], async (request, response) => 
+//     {
+//         let errors = validationResult(request)
+//         if (!errors.isEmpty()) {
+//             return response.status(400).json({ errors: errors.array() })
+//         }
 
-router.patch("/:id", (request, response) => {
-    let product = products.find(el => el.id == request.params.id)
-    
-    Object.keys(request.body).forEach(key => {
-        if (product.key) {
-            product.key == request.body[key]
-        }
-    })
+//         const { name, description, price } = request.body
 
-    if (request.body.name) {
-        product.name = request.body.name
-    }
-    if (request.body.description) {
-        product.description = request.body.description
-    }
-    if (request.body.price) {
-        product.price = request.body.price
-    }
+//         try {
+//             let product = await Product.findById(request.params.id)
+//                 if (product) {
+//                     response.send(product)
+//                     let newProduct = request.body
+//                     product.splice(index)
+//                 }
+//             } catch (err) {
+//                 console.error(err.message)
+//                 response.status(500).send('Server error')
+//             }
+
+//             let indexOfTheElement = await products.findIndex({ name })
+
+//             if (product) {
+//                 response.status(400).json({ errors: [ { msg: 'Product already exists' }] })
+//             }
+
+//             product = new Product({
+//                 name,
+//                 description,
+//                 price
+//             })
+
+//             await product.save()
+
+//             response.status(201).send(product)
+//         } catch (err) {
+//             console.error(err.message)
+//             response.status(500).send('Server error')
+//         }
+
+//         let indexOfTheElement = products.findIndex(element => element.id == request.params.id)
+//         let newProduct = request.body
+//         products.splice(indexOfTheElement, 1, newProduct)
+//         response.send(products[indexOfTheElement])
+// })
+
+// router.patch("/:id", (request, response) => {
+//     let product = products.find(el => el.id == request.params.id)
     
-    response.send(product)
-})
+//     Object.keys(request.body).forEach(key => {
+//         if (product.key) {
+//             product.key == request.body[key]
+//         }
+//     })
+
+//     if (request.body.name) {
+//         product.name = request.body.name
+//     }
+//     if (request.body.description) {
+//         product.description = request.body.description
+//     }
+//     if (request.body.price) {
+//         product.price = request.body.price
+//     }
+    
+//     response.send(product)
+// })
 
 // DELETE
 router.delete("/:id", (request, response) => {
-    let index = products.findIndex(el => el.id == request.params.id)
-    if (index != -1) {
-        products.splice(index, 1)
-        response.sendStatus(200)
-    } else {
-        response.sendStatus(404)
-    }
+    // let index = products.findByIdAndDelete(el => el.id == request.params.id)
+    // if (index != -1) {
+    //     products.splice(index, 1)
+    //     response.sendStatus(200)
+    // } else {
+    //     response.sendStatus(404)
+    // }
+    Product.findByIdAndDelete(request.params.id)
+        .then(confirmation => response.send(console.log(confirmation)))
+		.catch(error => response.send(error))
 })
 
 module.exports = router
