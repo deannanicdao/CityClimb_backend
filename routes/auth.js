6bbbfe7f557d6e4435d36c013cc0f0064f928e9a
deannanicdao <deanna.nicdao@gmail.com>
2021-01-31 20:34:17 +1000

    Test auth route now returns user object with json web token

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,21 @@
 // const { request, response } = require('express')
 const express = require('express')
 const router = express.Router()
-const auth = require('../middleware/auth')  
+const auth = require('../middleware/auth') 
+const User = require('./../models/User') 
 
 
 // @route   GET auth
 // @desc    Test route
 // @access  Public
-router.get("/", auth, (request, response) => response.send('Auth route'))
+router.get("/", auth, async (request, response) => {
+    try {
+        const user = await User.findById(request.user.id).select('-password')
+        response.json(user)
+    } catch (err) {
+        console.error(err.message)
+        response.status(500).send('Server Error')
+    }
+})
 
 module.exports = router
