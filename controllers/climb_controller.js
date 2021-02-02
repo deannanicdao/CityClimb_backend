1ac4030625891da5dc56555829202cf7fabd42e0
Jeremy Bell <jeremy.bell91@gmail.com>
2021-02-02 15:25:59 +1000

    It works. Can upload climbs with images that go to cloudinary

--- controllers/climb_controller.js
+++ controllers/climb_controller.js
@@ -4,51 +4,45 @@ import DatauriParser from 'datauri/parser.js'
 import path from 'path'
 import { uploader } from '../config/cloudinaryConfig.js'
 
-const create = async (req, res) =>{
+const create = (req, res) =>{
     console.log('Inside create')
-    // console.log(req.file)
+
     let { gym, wall, colour, youtubeUrl } = req.body
-    // console.log(gym, wall, colour, youtubeUrl)
+
 
     const parser = new DatauriParser()
     const fileExtension = path.extname(req.file.originalname).toString().toLowerCase()
-    // console.log(fileExtension)
     const bufferContent = req.file.buffer
-    // console.log(bufferContent)
     const file = parser.format(fileExtension, bufferContent).content
-    console.log(file)
-
-    // const image = req.file.path
+    // console.log(file)
 
     let video = getVideoId(youtubeUrl)
 
-    const image = await uploader.upload(file).url
+    uploader.upload(file,(url, err) => {
+        // console.log(1)
+        const image = url.url
+        let climb = new Climb({
+            gym,
+            wall,
+            colour,
+            image,
+            video
+        })
+        // console.log(2)
 
-
-
-    let climb = new Climb({
-        gym,
-        wall,
-        colour,
-        image,
-        video
-    })
-
-    console.log(climb)
-
-    climb.save((err, climb) => {
-        if(err){
-            return res.status(400).json({
-                errors: err.message
+        climb.save((err, climb) => {
+            if(err){
+                return res.status(400).json({
+                    errors: err.message
+                })
+            }
+    
+            res.status(200).json({
+                message: "Created climb successfully",
+                climb
             })
-        }
-
-        res.status(200).json({
-            message: "Created climb successfully",
-            climb
         })
     })
-
 }
 
 export default { 
